45d7b50efa6431f5cb31105f712fc4460746d3a9
HCM25_CPL_JAVA_01_TranGiaKiet <kiettgse180500@fpt.edu.vn>
2025-11-13 04:47:00 +0700

    Create V15__Add_Event_Additional_Fields.sql

---
+++ S-PACE_BE/src/main/resources/db/migration/V15__Add_Event_Additional_Fields.sql
@@ -0,0 +1,20 @@
+-- Add additional fields to event table
+-- These fields are: max_participants, requirements, contact_info
+
+-- Add max_participants column
+ALTER TABLE event 
+ADD COLUMN IF NOT EXISTS max_participants INTEGER;
+
+-- Add requirements column
+ALTER TABLE event 
+ADD COLUMN IF NOT EXISTS requirements TEXT;
+
+-- Add contact_info column
+ALTER TABLE event 
+ADD COLUMN IF NOT EXISTS contact_info VARCHAR(500);
+
+-- Add comments for documentation
+COMMENT ON COLUMN event.max_participants IS 'Maximum number of participants allowed for the event';
+COMMENT ON COLUMN event.requirements IS 'Requirements for participants to join the event';
+COMMENT ON COLUMN event.contact_info IS 'Contact information for the event';
+
